80fca442e4124de3a11c8282d74ce54dde064ca2
mishana <assembler@michaels-MacBook-Pro.local>
2013-09-30 02:33:29 +0300

    added a “menuTabBarViewController” which now enables to have the “monitor” tab (in the middle) as the initial tab

---
+++ Heartbeat/MenuTabBarViewController.m
@@ -0,0 +1,39 @@
+//
+//  MenuTabBarViewController.m
+//  Heartbeat
+//
+//  Created by michael leybovich on 9/30/13.
+//  Copyright (c) 2013 michael leybovich. All rights reserved.
+//
+
+#import "MenuTabBarViewController.h"
+
+@interface MenuTabBarViewController ()
+
+@end
+
+@implementation MenuTabBarViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+    
+    self.selectedIndex = 1;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
