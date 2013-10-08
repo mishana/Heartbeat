8d42333b138937357af903291f8806bc3a45199e
mishana <assembler@michaels-MacBook-Pro.local>
2013-10-08 16:44:42 +0300

    added Result class

---
+++ Heartbeat/ResultsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ResultsViewController.h
+//  Heartbeat
+//
+//  Created by michael leybovich on 10/8/13.
+//  Copyright (c) 2013 michael leybovich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ResultsViewController : UIViewController
+
+@end
